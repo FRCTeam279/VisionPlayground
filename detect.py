235b7021afc4c245bb7b89761db5aece908aa6b5
QueenHidari <sam.lehman@toleodtechnologyacademy.org>
2017-01-25 19:53:34 -0500

    I did the thing

--- detect.py
+++ detect.py
@@ -13,23 +13,69 @@ time.sleep(0.1)
 # camera.capture(imgArray, format="bgr", resize=(640,360), use_video_port=True)
 
 # img = imgArray.array
-img = cv2.imread('image1.png')
+img = cv2.imread('image6.png')
 contours = vis.process(img)
-rect1 = cv2.boundingRect(contours[0])
-rect2 = cv2.boundingRect(contours[1])
-print(str(rect1))
-print(str(rect2))
+
+rect1Exists = False
+rect2Exists = False
+
+if 0 < len(contours):
+    rect1 = cv2.boundingRect(contours[0])
+    print(str(rect1))
+    rect1Exists = True
+else:
+    print('contour not found')
+    rect1Exists = False
+
+if 1 < len(contours):
+    rect2 = cv2.boundingRect(contours[1])
+    print(str(rect2))
+    rect2Exists = True
+else:
+    print('contour not found')
+    rect2Exists = False
+
+# declaring pink color for drawing bounding rectangle
 color = (100,0,255)
-point11 = (rect1[0], rect1[1])
-print(str(point11))
-point21 = ((rect1[0] + rect1[2]), (rect1[1] + rect1[3]))
-print(str(point21))
-img = cv2.rectangle(img, point11, point21, color, thickness=2, lineType=8, shift=0)
-point12 = (rect2[0], rect2[1])
-print(str(point12))
-point22 = ((rect2[0] + rect2[2]), (rect2[1] + rect2[3]))
-img = cv2.rectangle(img, point12, point22, color, thickness=2, lineType=8, shift=0)
+if(rect1Exists == True):
+    # declaring rectangle objects
+    class rectObj1:
+        # top left point
+        pt1 = (rect1[0], rect1[1])
+        # bottom right point
+        pt2 = ((rect1[0] + rect1[2]), (rect1[1] + rect1[3]))
+        # width and height
+        w = rect1[2]
+        h = rect1[3]
+    
+    print(str(rectObj1.pt1))
+    print(str(rectObj1.pt2))
+    img = cv2.rectangle(img, rectObj1.pt1, rectObj1.pt2, color, thickness=2, lineType=8, shift=0)
+
+if(rect2Exists == True):    
+    class rectObj2:
+        # top left point
+        pt1 = (rect2[0], rect2[1])
+        # bottom right point
+        pt2 = ((rect2[0] + rect2[2]), (rect2[1] + rect2[3]))
+        # width and height
+        w = rect1[2]
+        h = rect1[3]
+    print(str(rectObj2.pt1))
+    print(str(rectObj2.pt2))
+    img = cv2.rectangle(img, rectObj2.pt1, rectObj2.pt2, color, thickness=2, lineType=8, shift=0)
 
 cv2.imwrite('output.png', img)
+
+if(rect1Exists == True) and (rect2Exists == True):
+    
+    if(rectObj1.pt1 < rectObj2.pt1):
+        leftRect = rectObj1
+        rightRect = rectObj2
+    else:
+        leftRect = rectObj2
+        rightRect = rectObj1
+
+
 
 # camera.close()
