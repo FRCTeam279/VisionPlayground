8ea8f16fc29ace26f4b6537441ee3868ffdd5318
Hidari <sam.lehman@toledotechnologyacademy.org>
2017-02-15 02:07:38 +0000

    Added test networktables script

--- autoDetect.py
+++ autoDetect.py
@@ -3,22 +3,18 @@ from pipeline import ContourPipeline
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from PIL import Image as PImage
-from PIL import ImageTk
-import tkinter
-from tkinter import *
-import imutils
 import time
 import numpy as np
 import cv2
 import sys
 import math
 
-NetworkTables.initialize(server='10.2.79.2')
+NetworkTables.initialize(server='10.2.79.101')
 print('Waiting to connect...')
 while NetworkTables.isConnected() == False:
     time.sleep(0.1)
 
-nt = NetworkTables.getTable("BLAHBLAHBLAH")
+nt = NetworkTables.getTable("Gear")
 imageHeight = 1232
 
 def getDFromW(width):
@@ -32,7 +28,9 @@ def getDFromWt(widthTotal):
 
 vis = ContourPipeline()
 
-imgArray = PiRGBArray(self.camera, size=(1640, 1232))
+
+self.camera = PiCamera()
+imgArray = PiRGBArray(camera, size=(1640, 1232))
 camera.resolution = (1640, 1232)
 camera.capture(imgArray, format="bgr", use_video_port=True)
 img = imgArray.array
@@ -76,41 +74,40 @@ if(rect2Exists == True):
         h = rect2[3]
 
 if(rect1Exists == True) and (rect2Exists == True):
-            if(rectObj1.pt1 < rectObj2.pt1):
-                leftRect = rectObj1
-                rightRect = rectObj2
-            else:
-                leftRect = rectObj2
-                rightRect = rectObj1
-            
-            leftDist = getDfromW(leftRect.w)
-            rightDist = getDfromW(rightRect.w)
-            
-            if(rightDist > leftDist):
-                nearDist = leftDist
-                farDist = rightDist
-            elif(rightDist < leftDist):
-                nearDist = rightDist
-                farDist = leftDist
-            else:
-                middleDist = leftDist
-                nearDist = middleDist
-                farDist = middleDist
-            
-            
-            if(nearDist != farDist):
-                middleDist = (farDist + nearDist) / 2
-                angle = math.degrees(math.acos((26.2656+(middleDist^2)-(farDist^2))/(2*5.125*middleDist))
-                if(leftDist < rightDist):
-                    angle *= -1
-                res = nt.putValue("angle", angle)
-                res1 = nt.putValue("distance", middleDist)
-                res2 = nt.putValue("eyes", True)
-            else:
-                res = nt.putValue("angle", 0)
-                res1 = nt.putValue("distance", middleDist)
-                res2 = nt.putValue("eyes", True)
-        else:
-            res = nt.putValue("angle", -999)
-            res1 = nt.putValue("distance", -999)
-            res2 = nt.putValue("eyes", False)
+    if(rectObj1.pt1 < rectObj2.pt1):
+        leftRect = rectObj1
+        rightRect = rectObj2
+    else:
+        leftRect = rectObj2
+        rightRect = rectObj1
+    
+    leftDist = getDfromW(leftRect.w)
+    rightDist = getDfromW(rightRect.w)
+    
+    if(rightDist > leftDist):
+        nearDist = leftDist
+        farDist = rightDist
+    elif(rightDist < leftDist):
+        nearDist = rightDist
+        farDist = leftDist
+    else:
+        middleDist = leftDist
+        nearDist = middleDist
+        farDist = middleDist
+    
+    if(nearDist != farDist):
+        middleDist = (farDist + nearDist) / 2
+        angle = math.degrees(math.acos((26.2656+(middleDist^2)-(farDist^2))/(2*5.125*middleDist)))
+        if leftDist < rightDist:
+            angle *= -1     
+        res = nt.putValue("angle", angle)
+        res1 = nt.putValue("distance", middleDist)
+        res2 = nt.putValue("eyes", True)
+    else:
+        res = nt.putValue("angle", 0)
+        res1 = nt.putValue("distance", middleDist)
+        res2 = nt.putValue("eyes", True)
+else:
+    res = nt.putValue("angle", -999)
+    res1 = nt.putValue("distance", -999)
+    res2 = nt.putValue("eyes", False)
